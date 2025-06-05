c98f056c1bf69f7a125f22e8f874d918c66823dd
jenetic <jznnylam@gmail.com>
2025-06-05 01:12:31 -0700

    Change variable placement

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -5,13 +5,16 @@ import googleCalendarPlugin from "@fullcalendar/google-calendar";
 
 // Configure FullCalendar CSS variables in the globals.css
 export default function Calendar() {
+  const PUBLIC_GOOGLE_CALENDAR_API_KEY = "AIzaSyDDp3PGxcPokT5ZUli1P_VPgE49JP396YM"
+  const PUBLIC_GOOGLE_CALENDAR_ID = "660d0f6a798426a94a177753ec35b4da7ea9e82c69fd4b81a95ca9183e05405a@group.calendar.google.com";
+
   return (
     <div className="text-sm sm:text-base">
       <FullCalendar
         plugins={[dayGridPlugin, googleCalendarPlugin]}
-        googleCalendarApiKey={process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}
+        googleCalendarApiKey={PUBLIC_GOOGLE_CALENDAR_API_KEY}
         events={{
-          googleCalendarId: process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_ID,
+          googleCalendarId: PUBLIC_GOOGLE_CALENDAR_ID,
         }}
         eventColor="var(--color-primary-medium)"
         fixedWeekCount={false}
